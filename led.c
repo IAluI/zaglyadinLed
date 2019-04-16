d88582b840fea58988966324560d7a206f048608
IAluI <29124424+ialui@users.noreply.github.com>
2019-04-16 18:50:00 +0700

    -Начало работы над универсальным генератором эффектов

--- led.c
+++ led.c
@@ -2,58 +2,55 @@
 #include <interrupt.h>
 #include <stdint.h>
 
-//#define F_CPU 8000000
+uint16_t velocity = 0;
+uint8_t velocityCounter = 0;
+uint8_t inverse = 0;
+uint8_t velocityChangePeriod = 366;
+int16_t a = 5000;
+void acceleration() {
+  int16_t diff = velocity + a;
+  if (diff >= 0xffff) {
+    inverse = !inverse;
+    a = -a;
+    velocity = velocity + a;
+  } else if (diff <= 0) {
+    inverse = !inverse;
+    a = -a;
+    velocity = -diff;
+  } else {
+    velocity = diff;
+  }
+}
 
-// Старший бит режим яркости, 2 младших бита режим скорости
-uint8_t settings = 0b000;
-uint8_t settingsCodes[8] = {
-    0b0000,
-    0b1000,
-    0b0001,
-    0b0101,
-    0b1001,
-    0b0010,
-    0b0110,
-    0b1010
-};
-// Массив скоростей
-uint16_t speed[4] = {
-    0x3333,
-    0x8000,
-    0xA666
-};
-// Массив яркостей
-uint8_t brightnessShift = 0;
-uint8_t brightness[9] = {
+typedef struct pinAddresses_t {
+    uint8_t *port;
+    uint8_t pin;
+} pinAddresses;
+pinAddresses ledAddresses[12];
+// Размер массива адресов диодов
+uint8_t ledAddressesLen = sizeof(ledAddresses) / sizeof(pinAddresses);
+// Константа определяющая сдвиг массива яркостей относительно массива адресов
+uint8_t ledAddressShift = 0;
+
+//Массив яркостей светодиодов
+uint8_t ledBrightness[ledAddressesLen] = {
     0b11111111,
     0b10000000,
     0b00011001,
-
-    0b11111111,
-    0b10011001,
-    0b00110011,
-
-    0b11111111,
-    0b10110011,
-    0b01001101
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000
 };
 uint8_t brightnessMask = 0b00000001;
 
-typedef struct pinAdreses_t {
-    uint8_t *port;
-    uint8_t pin;
-} pinAdreses;
-pinAdreses ledAdreses[12];
-// Размер массива адресов диодов
-uint8_t ledAdresesLen = sizeof(ledAdreses) / sizeof(pinAdreses);
-
-// Функция чтения состояния ключей
-uint8_t getInputValue(uint8_t port) {
-  return (port & 0b00111000) >> 3;
-}
-
 // Вспомогательная функция для реализации модуляции
-void setBrightness(pinAdreses *ledAdr, uint8_t brightnessIndex) {
+void setBrightness(pinAddresses *ledAdr, uint8_t brightnessIndex) {
   if (brightness[brightnessIndex] & brightnessMask) {
     *ledAdr->port |= ledAdr->pin;
   } else {
@@ -61,34 +58,32 @@ void setBrightness(pinAdreses *ledAdr, uint8_t brightnessIndex) {
   }
 }
 
-uint8_t counter = 0;
-
 void main(void) {
   // Инициализируем массив структур ханящих адрес порта и маску для выставление нужного бита данного порта в 1
-  ledAdreses[0].port = &PORTD;
-  ledAdreses[0].pin = (1 << 2);
-  ledAdreses[1].port = &PORTA;
-  ledAdreses[1].pin = (1 << 0);
-  ledAdreses[2].port = &PORTA;
-  ledAdreses[2].pin = (1 << 1);
-  ledAdreses[3].port = &PORTD;
-  ledAdreses[3].pin = (1 << 6);
-  ledAdreses[4].port = &PORTB;
-  ledAdreses[4].pin = (1 << 0);
-  ledAdreses[5].port = &PORTB;
-  ledAdreses[5].pin = (1 << 1);
-  ledAdreses[6].port = &PORTB;
-  ledAdreses[6].pin = (1 << 2);
-  ledAdreses[7].port = &PORTB;
-  ledAdreses[7].pin = (1 << 3);
-  ledAdreses[8].port = &PORTB;
-  ledAdreses[8].pin = (1 << 4);
-  ledAdreses[9].port = &PORTB;
-  ledAdreses[9].pin = (1 << 5);
-  ledAdreses[10].port = &PORTB;
-  ledAdreses[10].pin = (1 << 6);
-  ledAdreses[11].port = &PORTB;
-  ledAdreses[11].pin = (1 << 7);
+  ledAddresses[0].port = &PORTD;
+  ledAddresses[0].pin = (1 << 2);
+  ledAddresses[1].port = &PORTA;
+  ledAddresses[1].pin = (1 << 0);
+  ledAddresses[2].port = &PORTA;
+  ledAddresses[2].pin = (1 << 1);
+  ledAddresses[3].port = &PORTD;
+  ledAddresses[3].pin = (1 << 6);
+  ledAddresses[4].port = &PORTB;
+  ledAddresses[4].pin = (1 << 0);
+  ledAddresses[5].port = &PORTB;
+  ledAddresses[5].pin = (1 << 1);
+  ledAddresses[6].port = &PORTB;
+  ledAddresses[6].pin = (1 << 2);
+  ledAddresses[7].port = &PORTB;
+  ledAddresses[7].pin = (1 << 3);
+  ledAddresses[8].port = &PORTB;
+  ledAddresses[8].pin = (1 << 4);
+  ledAddresses[9].port = &PORTB;
+  ledAddresses[9].pin = (1 << 5);
+  ledAddresses[10].port = &PORTB;
+  ledAddresses[10].pin = (1 << 6);
+  ledAddresses[11].port = &PORTB;
+  ledAddresses[11].pin = (1 << 7);
 
   // Конфигурируем и инициализируем порты ввода вывода
   DDRA = 0b011;
@@ -110,7 +105,7 @@ void main(void) {
   // Настройка таймера1
   TCCR1A = 0b00000000; // настрока таймера
   //TCCR1B = 0b00000000;
-  TCNT1 = 0b0;
+  TCNT1 = velocity;
 
   TCCR0B = 0b00000100; // Запуск таймера0 с делителем 256
 
@@ -120,30 +115,29 @@ void main(void) {
 }
 
 ISR(TIM0_OVF) {
-    uint8_t i;
-    // Сохраняем текущее состояние счетчика в буфер
-    uint8_t buff = counter;
-    // Модуляция
-    brightnessMask = (brightnessMask >> 1) | (brightnessMask << 7);
-    TCNT0 = ~brightnessMask;
-    // Зажигаем диоды
-    for (i = 0; i < 3; i++) {
-      setBrightness(&ledAdreses[(buff + 3 - i) % 12], i + brightnessShift * 3);
+  uint8_t i;
+  // Модуляция
+  brightnessMask = (brightnessMask >> 1) | (brightnessMask << 7);
+  TCNT0 = ~brightnessMask;
+  // Зажигаем диоды
+  uint8_t bShft = ledAddressesLen - ledAddressShift;
+  for (i = 0; i < ledAddressShift; i++) {
+    setBrightness(&ledAddresses[i], ledBrightness[ledAddressesLen * inverse + (1 - 2 * inverse) * (i + bShft)]);
+  }
+  for (i = ledAddressShift; i < ledAddressesLen; i++) {
+    setBrightness(&ledAddresses[i], ledBrightness[ledAddressesLen * inverse + (1 - 2 * inverse) * (i - ledAddressShift)]);
+  }
+
+  if (brightnessMask == 0b00000001) {
+    if (++velocityCounter == velocityChangePeriod) {
+      acceleration();
     }
+  }
 }
 
 ISR(TIM1_OVF) {
-    // Считываем состояние ключей
-    settings = getInputValue(PIND);
-    // Выставляем начальное значение счетчика в соответсвии с выбранной скоростью
-    TCNT1 = speed[settingsCodes[settings] & 0b011];
-    // Сохраняем настройки яркости
-    brightnessShift = settingsCodes[settings] >> 2;
-
-    // Обнуляем счетчик
-    if (++counter == ledAdresesLen) {
-      counter = 0;
-    }
-    // Гасим диод идущий за последним горящим
-    *(ledAdreses[counter % 12].port) &= ~ledAdreses[counter].pin;
+  // Обнуляем счетчик
+  if (++ledAddressShift == ledAddressesLen) {
+    ledAddressShift = 0;
+  }
 }
